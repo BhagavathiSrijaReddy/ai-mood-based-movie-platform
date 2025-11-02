2eddac491710ff587b5fcda28d359ee1dc749a12
Mittyskull0107 <maitreyindaram@gmail.com>
2025-11-02 21:36:56 +0530

    .

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
   createServer((req, res) => {
     handle(req, res);
-  }).listen(process.env.PORT || 3000, '0.0.0.0', () => {
+  }).listen(process.env.PORT || 10000, '0.0.0.0', () => {
     console.log('Server running on port', process.env.PORT || 3000);
   });
 });
